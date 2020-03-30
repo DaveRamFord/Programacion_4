18c59e2d0b111b7f3df3699f36d37bb8832dd703
usis062918 <ergarcia194@gmail.com>
2020-03-30 00:10:15 -0600

    tarde pero completo

--- app_academica/Private/conexion/DB.php
+++ app_academica/Private/conexion/DB.php
@@ -1,16 +1,12 @@
 <?php
-
-/**
- * Clase de conexion al servidor de BD
- */
 class Conexion{
-    private $conexion='', $result='';
+    private $Conexion='', $result='';
 
-    public function Conexion($server, $user, $pass, $db){
+    public function Conexion($server, $user, $pass, $db="db_app_academica"){
         $this->conexion = mysqli_connect($server,$user,$pass,$db) or die('NO pude conectarme al servidor de BD');
     }
     public function consultas($sql=''){
-        $this->result = mysqli_query($sql->Conexion,$sql) or die(mysqli_error($this->conexion));
+        $this->result = mysqli_query($this->conexion,$sql) or die(mysqli_error($this->conexion));
     }
     public function obtener_datos(){
         return $this->result->fetch_all(MYSQLI_ASSOC);
